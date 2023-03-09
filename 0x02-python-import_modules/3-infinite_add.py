f49684e235661e243c1e53898a9431a290856276
Love-Becca <follycube2020@gmail.com>
2023-03-09 12:06:43 -0800

    correction

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-        import sys
-        arguments = sys.argv
-        sum = 0
-        if len(arguments) == 1:
-            print("{}".format(sum))
-        else:
-            for i in range(1, len(arguments)):
-                sum = sum + int(arguments[i])
-            print("{}".format(sum))
+    import sys
+    arguments = sys.argv
+    sum = 0
+    if len(arguments) == 1:
+        print("{}".format(sum))
+    else:
+        for i in range(1, len(arguments)):
+            sum +=int(arguments[i])
+        print("{}".format(sum))
