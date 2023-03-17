480e322823ff7030230e1e77f8a6239e5c88be48
Love-Becca <follycube2020@gmail.com>
2023-03-16 17:37:37 -0700

    by_2

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    temp_dic = a_dictionary
+    temp_dic = a_dictionary.copy()
     for i in temp_dic.keys():
         temp_dic[i] *= 2
     return temp_dic
